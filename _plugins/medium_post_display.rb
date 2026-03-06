5f61988ecc55327e8692eac340c8f494b9032949
hasantayyar <tayyar.besik@gmail.com>
2026-03-06 17:33:27 +0100

    feat: paging

--- _plugins/medium_post_display.rb
+++ _plugins/medium_post_display.rb
@@ -1,26 +1,34 @@
-require 'feedjira'
-require 'jekyll'
-module Jekyll
-  class JekyllDisplayMediumPosts < Generator
-    safe true
-    priority :high
-def generate(site)
-      jekyll_coll = Jekyll::Collection.new(site, 'medium_posts')
-      site.collections['medium_posts'] = jekyll_coll
-Feedjira::Feed.fetch_and_parse("https://medium.com/feed/@" + ENV["MEDIUM_USERNAME"]).entries.each do |e|
-        p "Title: #{e.title}, published on Medium #{e.url} #{e}"
-        title = e[:title]
-        content = e[:content]
-        guid = e[:url]
-        date = e[:pubDate]
-        path = "./medium_posts/" + title + ".md"
-        path = site.in_source_dir(path)
-        doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
-        doc.data['title'] = title;
-        doc.data['feed_content'] = content;
-        doc.data['medium_link'] = e.url;
-        doc.data['medium_pub_date'] = e[:pubDate];
-        jekyll_coll.docs << doc
+begin
+  require 'feedjira'
+rescue LoadError => e
+  Jekyll.logger.warn "Medium posts:", "Skipping (feedjira/nokogiri not available: #{e.message})"
+end
+
+if defined?(Feedjira)
+  require 'jekyll'
+  module Jekyll
+    class JekyllDisplayMediumPosts < Generator
+      safe true
+      priority :high
+
+      def generate(site)
+        jekyll_coll = Jekyll::Collection.new(site, 'medium_posts')
+        site.collections['medium_posts'] = jekyll_coll
+        username = ENV["MEDIUM_USERNAME"].to_s
+        return if username.empty?
+        Feedjira::Feed.fetch_and_parse("https://medium.com/feed/@" + username).entries.each do |e|
+          Jekyll.logger.debug "Medium:", "Title: #{e.title}, published on Medium #{e.url}"
+          title = e[:title]
+          content = e[:content]
+          path = "./medium_posts/" + title + ".md"
+          path = site.in_source_dir(path)
+          doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
+          doc.data['title'] = title
+          doc.data['feed_content'] = content
+          doc.data['medium_link'] = e.url
+          doc.data['medium_pub_date'] = e[:pubDate]
+          jekyll_coll.docs << doc
+        end
       end
     end
   end
